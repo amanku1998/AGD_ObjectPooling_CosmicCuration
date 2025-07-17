8071cf046b493d7d6ace145f5ff08854a64cc8b3
Aman Kumar <aman.kumar01747@gmail.com>
2025-07-17 18:48:14 +0530

    -Create a get bullet method

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -19,16 +19,14 @@ namespace CosmicCuration.Player
         private ShieldState currentShieldState;
         private int currentHealth;
         private float currentRateOfFire;
+        private BulletPool bulletPool;
 
-
-        public PlayerController(PlayerView playerViewPrefab, PlayerScriptableObject playerScriptableObject, BulletView bulletPrefab, BulletScriptableObject bulletScriptableObject)
+        public PlayerController(PlayerView playerViewPrefab, PlayerScriptableObject playerScriptableObject, BulletPool bulletPool)
         {
             playerView = Object.Instantiate(playerViewPrefab);
             playerView.SetController(this);
             this.playerScriptableObject = playerScriptableObject;
-            this.bulletPrefab = bulletPrefab;
-            this.bulletScriptableObject = bulletScriptableObject;
-
+            this.bulletPool = bulletPool;
             InitializeVariables();
         }
 
@@ -96,7 +94,7 @@ namespace CosmicCuration.Player
 
         private void FireBulletAtPosition(Transform fireLocation)
         {
-            BulletController bulletToFire = new BulletController(bulletPrefab, bulletScriptableObject);
+            BulletController bulletToFire = bulletPool.GetBullet();
             bulletToFire.ConfigureBullet(fireLocation);
             GameService.Instance.GetSoundService().PlaySoundEffects(SoundType.PlayerBullet);
         }
