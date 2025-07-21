83bfeb312dcc5e7bff8c9bd16d643fa6f0088285
Aman Kumar <aman.kumar01747@gmail.com>
2025-07-21 20:19:31 +0530

    -Make VFX pool

---
+++ Assets/Scripts/VFX/VFXPool.cs
@@ -0,0 +1,21 @@
+using CosmicCuration.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CosmicCuration.VFX
+{
+    public class VFXPool : GenericObjectPool<VFXController>
+    {
+        private VFXView vfxPrefab;
+
+        public VFXPool(VFXView vfxPrefab)
+        {
+            this.vfxPrefab = vfxPrefab;
+        }
+
+        public VFXController GetVFX() => GetItem<VFXController>();
+
+        protected override VFXController CreateItem<T>() => new VFXController(vfxPrefab);
+    }
+}
--- Assets/Scripts/VFX/VFXService.cs
+++ Assets/Scripts/VFX/VFXService.cs
@@ -5,15 +5,31 @@ namespace CosmicCuration.VFX
 {
     public class VFXService
     {
-        private List<VFXData> vfxData = new List<VFXData>();
+        //private List<VFXData> vfxData = new List<VFXData>();
 
-        public VFXService(VFXScriptableObject vfxScriptableObject) => vfxData = vfxScriptableObject.vfxData;
+        private VFXPool VFXPool;
+        //public VFXService(VFXScriptableObject vfxScriptableObject)
+        //{
+        //    vfxData = vfxScriptableObject.vfxData;
+
+        //}
+
+        public VFXService(VFXView vfxPrefab)
+        {
+            VFXPool = new VFXPool(vfxPrefab);
+        }
 
         public void PlayVFXAtPosition(VFXType type, Vector2 spawnPosition)
         {
-            VFXView prefabToSpawn = vfxData.Find(item => item.type == type).prefab;
-            VFXController vfxToPlay = new VFXController(prefabToSpawn);
-            vfxToPlay.Configure(spawnPosition);
+            //VFXView prefabToSpawn = vfxData.Find(item => item.type == type).prefab;
+            //VFXController vfxToPlay = new VFXController(prefabToSpawn);
+            VFXController vfxToPlay = VFXPool.GetVFX();
+            vfxToPlay.Configure(type, spawnPosition);
         }
-    } 
+
+        public void ReturnVFXToPool(VFXController vfxController)
+        {
+            VFXPool.ReturnItem(vfxController);
+        }
+    }
 }
